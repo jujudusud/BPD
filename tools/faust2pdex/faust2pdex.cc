3c9283335f88bbd676d6447b5b92369fbb7a0c50
JP Cimalando <jp-dev@inbox.ru>
2018-12-18 19:25:20 +0100

    ajout outil: faust2pdex

---
+++ tools/faust2pdex/faust2pdex.cc
@@ -0,0 +1,64 @@
+#include "faust2pdex.h"
+#include "faustdata.h"
+#include "main_window.h"
+#include <QMessageBox>
+#include <QJsonDocument>
+#include <QProcess>
+#include <QFile>
+#include <QFileInfo>
+
+struct Application::Impl {
+    MainWindow *win_;
+};
+
+Application::Application(int &argc, char *argv[])
+    : QApplication(argc, argv), P(new Impl)
+{
+    MainWindow *win = P->win_ = new MainWindow;
+    win->setWindowTitle(applicationName());
+    win->show();
+}
+
+Application::~Application()
+{
+}
+
+void Application::loadFaustFile(const QString &filename)
+{
+    int exitcode = QProcess::execute("faust", {"-json", filename});
+    if (exitcode != 0) {
+        QMessageBox::warning(nullptr, tr("Error"), "Could not load Faust file.");
+        return;
+    }
+
+    QFile file(filename + ".json");
+    file.open(QIODevice::ReadOnly);
+    QJsonDocument doc = QJsonDocument::fromJson(file.readAll());
+    file.close();
+
+    if (doc.isNull()) {
+        QMessageBox::warning(nullptr, tr("Error"), "Could not read JSON data.");
+        return;
+    }
+
+    QString filebase = QFileInfo(filename).baseName();
+    FaustData data = FaustData::loadJson(filebase, doc);
+    P->win_->loadFaustData(data);
+}
+
+int main(int argc, char *argv[])
+{
+    Application app(argc, argv);
+    QStringList args = app.arguments();
+    switch (args.size()) {
+    case 2:
+        app.loadFaustFile(args[1]);
+        break;
+    case 1:
+        break;
+    default:
+        return 1;
+    }
+    int ret = app.exec();
+    return ret;
+}
